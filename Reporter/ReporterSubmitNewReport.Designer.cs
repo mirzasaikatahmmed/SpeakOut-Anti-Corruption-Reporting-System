f6781a38a892070dd1090fd10d46a343e1f813dc
MIRZA SAIKAT AHMMED <mirzasaikatahmmed@gmail.com>
2025-06-04 00:14:50 +0600

    Add UI for report submission and file selection
    Updated `ReporterSubmitNewReport.Designer.cs` to include a new user interface with controls for submitting reports, including panels, labels, text boxes, check boxes, buttons, and a list box for evidence.
    
    Modified `ReporterSubmitNewReport.cs` to introduce a private list `evidenceFiles` for storing selected file paths. Added `chooseFileBTN_Click` event handler to enable users to select multiple files through a dialog, which are then displayed in the evidence list box.

--- Reporter/ReporterSubmitNewReport.Designer.cs
+++ Reporter/ReporterSubmitNewReport.Designer.cs
@@ -28,7 +28,290 @@
         /// </summary>
         private void InitializeComponent()
         {
+            this.panel1 = new System.Windows.Forms.Panel();
+            this.panel3 = new System.Windows.Forms.Panel();
+            this.submitNone = new System.Windows.Forms.CheckBox();
+            this.submitAnonymously = new System.Windows.Forms.CheckBox();
+            this.label8 = new System.Windows.Forms.Label();
+            this.evidenceTextBox = new System.Windows.Forms.TextBox();
+            this.chooseFileBTN = new System.Windows.Forms.Button();
+            this.typeOfCorruptionComboBox = new System.Windows.Forms.ComboBox();
+            this.label7 = new System.Windows.Forms.Label();
+            this.label6 = new System.Windows.Forms.Label();
+            this.label5 = new System.Windows.Forms.Label();
+            this.reportDescriptionRichTextBox = new System.Windows.Forms.RichTextBox();
+            this.label4 = new System.Windows.Forms.Label();
+            this.dateofIncidentDateTimePicker = new System.Windows.Forms.DateTimePicker();
+            this.label3 = new System.Windows.Forms.Label();
+            this.locationTextBox = new System.Windows.Forms.TextBox();
+            this.label2 = new System.Windows.Forms.Label();
+            this.reportTitleTextBox = new System.Windows.Forms.TextBox();
+            this.newReportSubmitBTN = new System.Windows.Forms.Button();
+            this.panel2 = new System.Windows.Forms.Panel();
+            this.backPictureBoxBTN = new System.Windows.Forms.PictureBox();
+            this.label1 = new System.Windows.Forms.Label();
+            this.evidenceListBox = new System.Windows.Forms.ListBox();
+            this.panel1.SuspendLayout();
+            this.panel3.SuspendLayout();
+            this.panel2.SuspendLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.backPictureBoxBTN)).BeginInit();
             this.SuspendLayout();
+            // 
+            // panel1
+            // 
+            this.panel1.Controls.Add(this.panel3);
+            this.panel1.Controls.Add(this.panel2);
+            this.panel1.Location = new System.Drawing.Point(12, 12);
+            this.panel1.Name = "panel1";
+            this.panel1.Size = new System.Drawing.Size(526, 461);
+            this.panel1.TabIndex = 2;
+            // 
+            // panel3
+            // 
+            this.panel3.Controls.Add(this.evidenceListBox);
+            this.panel3.Controls.Add(this.submitNone);
+            this.panel3.Controls.Add(this.submitAnonymously);
+            this.panel3.Controls.Add(this.label8);
+            this.panel3.Controls.Add(this.evidenceTextBox);
+            this.panel3.Controls.Add(this.chooseFileBTN);
+            this.panel3.Controls.Add(this.typeOfCorruptionComboBox);
+            this.panel3.Controls.Add(this.label7);
+            this.panel3.Controls.Add(this.label6);
+            this.panel3.Controls.Add(this.label5);
+            this.panel3.Controls.Add(this.reportDescriptionRichTextBox);
+            this.panel3.Controls.Add(this.label4);
+            this.panel3.Controls.Add(this.dateofIncidentDateTimePicker);
+            this.panel3.Controls.Add(this.label3);
+            this.panel3.Controls.Add(this.locationTextBox);
+            this.panel3.Controls.Add(this.label2);
+            this.panel3.Controls.Add(this.reportTitleTextBox);
+            this.panel3.Controls.Add(this.newReportSubmitBTN);
+            this.panel3.Location = new System.Drawing.Point(3, 33);
+            this.panel3.Name = "panel3";
+            this.panel3.Size = new System.Drawing.Size(520, 425);
+            this.panel3.TabIndex = 1;
+            // 
+            // submitNone
+            // 
+            this.submitNone.AutoSize = true;
+            this.submitNone.Location = new System.Drawing.Point(318, 263);
+            this.submitNone.Name = "submitNone";
+            this.submitNone.Size = new System.Drawing.Size(62, 20);
+            this.submitNone.TabIndex = 24;
+            this.submitNone.Text = "None";
+            this.submitNone.UseVisualStyleBackColor = true;
+            // 
+            // submitAnonymously
+            // 
+            this.submitAnonymously.AutoSize = true;
+            this.submitAnonymously.Location = new System.Drawing.Point(318, 237);
+            this.submitAnonymously.Name = "submitAnonymously";
+            this.submitAnonymously.Size = new System.Drawing.Size(110, 20);
+            this.submitAnonymously.TabIndex = 23;
+            this.submitAnonymously.Text = "Anonymously";
+            this.submitAnonymously.UseVisualStyleBackColor = true;
+            // 
+            // label8
+            // 
+            this.label8.AutoSize = true;
+            this.label8.Font = new System.Drawing.Font("Century Gothic", 10.2F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label8.ForeColor = System.Drawing.Color.FromArgb(((int)(((byte)(41)))), ((int)(((byte)(128)))), ((int)(((byte)(185)))));
+            this.label8.Location = new System.Drawing.Point(314, 215);
+            this.label8.Name = "label8";
+            this.label8.Size = new System.Drawing.Size(73, 19);
+            this.label8.TabIndex = 22;
+            this.label8.Text = "Identity:";
+            // 
+            // evidenceTextBox
+            // 
+            this.evidenceTextBox.BackColor = System.Drawing.SystemColors.Window;
+            this.evidenceTextBox.BorderStyle = System.Windows.Forms.BorderStyle.None;
+            this.evidenceTextBox.Font = new System.Drawing.Font("Century Gothic", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.evidenceTextBox.ForeColor = System.Drawing.Color.FromArgb(((int)(((byte)(41)))), ((int)(((byte)(128)))), ((int)(((byte)(185)))));
+            this.evidenceTextBox.Location = new System.Drawing.Point(7, 325);
+            this.evidenceTextBox.Name = "evidenceTextBox";
+            this.evidenceTextBox.Size = new System.Drawing.Size(345, 25);
+            this.evidenceTextBox.TabIndex = 21;
+            // 
+            // chooseFileBTN
+            // 
+            this.chooseFileBTN.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(41)))), ((int)(((byte)(128)))), ((int)(((byte)(185)))));
+            this.chooseFileBTN.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.chooseFileBTN.Font = new System.Drawing.Font("Century Gothic", 10.2F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.chooseFileBTN.ForeColor = System.Drawing.Color.White;
+            this.chooseFileBTN.Location = new System.Drawing.Point(358, 321);
+            this.chooseFileBTN.Name = "chooseFileBTN";
+            this.chooseFileBTN.Size = new System.Drawing.Size(119, 33);
+            this.chooseFileBTN.TabIndex = 20;
+            this.chooseFileBTN.Text = "Choose File";
+            this.chooseFileBTN.UseVisualStyleBackColor = false;
+            this.chooseFileBTN.Click += new System.EventHandler(this.chooseFileBTN_Click);
+            // 
+            // typeOfCorruptionComboBox
+            // 
+            this.typeOfCorruptionComboBox.FormattingEnabled = true;
+            this.typeOfCorruptionComboBox.Items.AddRange(new object[] {
+            "Bribery",
+            "etc"});
+            this.typeOfCorruptionComboBox.Location = new System.Drawing.Point(318, 172);
+            this.typeOfCorruptionComboBox.Name = "typeOfCorruptionComboBox";
+            this.typeOfCorruptionComboBox.Size = new System.Drawing.Size(198, 24);
+            this.typeOfCorruptionComboBox.TabIndex = 19;
+            // 
+            // label7
+            // 
+            this.label7.AutoSize = true;
+            this.label7.Font = new System.Drawing.Font("Century Gothic", 10.2F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label7.ForeColor = System.Drawing.Color.FromArgb(((int)(((byte)(41)))), ((int)(((byte)(128)))), ((int)(((byte)(185)))));
+            this.label7.Location = new System.Drawing.Point(3, 298);
+            this.label7.Name = "label7";
+            this.label7.Size = new System.Drawing.Size(159, 19);
+            this.label7.TabIndex = 18;
+            this.label7.Text = "Upload Evidence: ";
+            // 
+            // label6
+            // 
+            this.label6.AutoSize = true;
+            this.label6.Font = new System.Drawing.Font("Century Gothic", 10.2F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label6.ForeColor = System.Drawing.Color.FromArgb(((int)(((byte)(41)))), ((int)(((byte)(128)))), ((int)(((byte)(185)))));
+            this.label6.Location = new System.Drawing.Point(314, 139);
+            this.label6.Name = "label6";
+            this.label6.Size = new System.Drawing.Size(163, 19);
+            this.label6.TabIndex = 17;
+            this.label6.Text = "Type of Corruption:";
+            // 
+            // label5
+            // 
+            this.label5.AutoSize = true;
+            this.label5.Font = new System.Drawing.Font("Century Gothic", 10.2F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label5.ForeColor = System.Drawing.Color.FromArgb(((int)(((byte)(41)))), ((int)(((byte)(128)))), ((int)(((byte)(185)))));
+            this.label5.Location = new System.Drawing.Point(314, 74);
+            this.label5.Name = "label5";
+            this.label5.Size = new System.Drawing.Size(83, 19);
+            this.label5.TabIndex = 16;
+            this.label5.Text = "Location:";
+            // 
+            // reportDescriptionRichTextBox
+            // 
+            this.reportDescriptionRichTextBox.Location = new System.Drawing.Point(7, 96);
+            this.reportDescriptionRichTextBox.Name = "reportDescriptionRichTextBox";
+            this.reportDescriptionRichTextBox.Size = new System.Drawing.Size(285, 181);
+            this.reportDescriptionRichTextBox.TabIndex = 15;
+            this.reportDescriptionRichTextBox.Text = "";
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Font = new System.Drawing.Font("Century Gothic", 10.2F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label4.ForeColor = System.Drawing.Color.FromArgb(((int)(((byte)(41)))), ((int)(((byte)(128)))), ((int)(((byte)(185)))));
+            this.label4.Location = new System.Drawing.Point(3, 74);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(105, 19);
+            this.label4.TabIndex = 14;
+            this.label4.Text = "Description:";
+            // 
+            // dateofIncidentDateTimePicker
+            // 
+            this.dateofIncidentDateTimePicker.Location = new System.Drawing.Point(316, 38);
+            this.dateofIncidentDateTimePicker.Name = "dateofIncidentDateTimePicker";
+            this.dateofIncidentDateTimePicker.Size = new System.Drawing.Size(200, 22);
+            this.dateofIncidentDateTimePicker.TabIndex = 13;
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Font = new System.Drawing.Font("Century Gothic", 10.2F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label3.ForeColor = System.Drawing.Color.FromArgb(((int)(((byte)(41)))), ((int)(((byte)(128)))), ((int)(((byte)(185)))));
+            this.label3.Location = new System.Drawing.Point(314, 13);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(52, 19);
+            this.label3.TabIndex = 12;
+            this.label3.Text = "Date:";
+            // 
+            // locationTextBox
+            // 
+            this.locationTextBox.BackColor = System.Drawing.SystemColors.Window;
+            this.locationTextBox.BorderStyle = System.Windows.Forms.BorderStyle.None;
+            this.locationTextBox.Font = new System.Drawing.Font("Century Gothic", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.locationTextBox.ForeColor = System.Drawing.Color.FromArgb(((int)(((byte)(41)))), ((int)(((byte)(128)))), ((int)(((byte)(185)))));
+            this.locationTextBox.Location = new System.Drawing.Point(316, 96);
+            this.locationTextBox.Name = "locationTextBox";
+            this.locationTextBox.Size = new System.Drawing.Size(200, 25);
+            this.locationTextBox.TabIndex = 11;
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Font = new System.Drawing.Font("Century Gothic", 10.2F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label2.ForeColor = System.Drawing.Color.FromArgb(((int)(((byte)(41)))), ((int)(((byte)(128)))), ((int)(((byte)(185)))));
+            this.label2.Location = new System.Drawing.Point(3, 13);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(106, 19);
+            this.label2.TabIndex = 10;
+            this.label2.Text = "Report Title: ";
+            // 
+            // reportTitleTextBox
+            // 
+            this.reportTitleTextBox.BackColor = System.Drawing.SystemColors.Window;
+            this.reportTitleTextBox.BorderStyle = System.Windows.Forms.BorderStyle.None;
+            this.reportTitleTextBox.Font = new System.Drawing.Font("Century Gothic", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.reportTitleTextBox.ForeColor = System.Drawing.Color.FromArgb(((int)(((byte)(41)))), ((int)(((byte)(128)))), ((int)(((byte)(185)))));
+            this.reportTitleTextBox.Location = new System.Drawing.Point(7, 35);
+            this.reportTitleTextBox.Name = "reportTitleTextBox";
+            this.reportTitleTextBox.Size = new System.Drawing.Size(285, 25);
+            this.reportTitleTextBox.TabIndex = 3;
+            // 
+            // newReportSubmitBTN
+            // 
+            this.newReportSubmitBTN.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(41)))), ((int)(((byte)(128)))), ((int)(((byte)(185)))));
+            this.newReportSubmitBTN.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.newReportSubmitBTN.Font = new System.Drawing.Font("Century Gothic", 10.2F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.newReportSubmitBTN.ForeColor = System.Drawing.Color.White;
+            this.newReportSubmitBTN.Location = new System.Drawing.Point(358, 373);
+            this.newReportSubmitBTN.Name = "newReportSubmitBTN";
+            this.newReportSubmitBTN.Size = new System.Drawing.Size(148, 35);
+            this.newReportSubmitBTN.TabIndex = 8;
+            this.newReportSubmitBTN.Text = "Submit";
+            this.newReportSubmitBTN.UseVisualStyleBackColor = false;
+            // 
+            // panel2
+            // 
+            this.panel2.Controls.Add(this.backPictureBoxBTN);
+            this.panel2.Controls.Add(this.label1);
+            this.panel2.Location = new System.Drawing.Point(3, 3);
+            this.panel2.Name = "panel2";
+            this.panel2.Size = new System.Drawing.Size(520, 24);
+            this.panel2.TabIndex = 0;
+            // 
+            // backPictureBoxBTN
+            // 
+            this.backPictureBoxBTN.Image = global::SpeakOut___Anti_Corruption_Reporting_System.Properties.Resources.back;
+            this.backPictureBoxBTN.Location = new System.Drawing.Point(491, -1);
+            this.backPictureBoxBTN.Name = "backPictureBoxBTN";
+            this.backPictureBoxBTN.Size = new System.Drawing.Size(25, 25);
+            this.backPictureBoxBTN.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage;
+            this.backPictureBoxBTN.TabIndex = 9;
+            this.backPictureBoxBTN.TabStop = false;
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("Century Gothic", 10.2F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label1.ForeColor = System.Drawing.Color.FromArgb(((int)(((byte)(41)))), ((int)(((byte)(128)))), ((int)(((byte)(185)))));
+            this.label1.Location = new System.Drawing.Point(3, 2);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(163, 19);
+            this.label1.TabIndex = 0;
+            this.label1.Text = "Submit New Report";
+            // 
+            // evidenceListBox
+            // 
+            this.evidenceListBox.FormattingEnabled = true;
+            this.evidenceListBox.ItemHeight = 16;
+            this.evidenceListBox.Location = new System.Drawing.Point(7, 356);
+            this.evidenceListBox.Name = "evidenceListBox";
+            this.evidenceListBox.Size = new System.Drawing.Size(345, 52);
+            this.evidenceListBox.TabIndex = 25;
             // 
             // ReporterSubmitNewReport
             // 
@@ -36,13 +319,44 @@
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.ClientSize = new System.Drawing.Size(550, 485);
             this.ControlBox = false;
+            this.Controls.Add(this.panel1);
             this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.None;
             this.Name = "ReporterSubmitNewReport";
             this.Text = "ReporterSubmitNewReport";
+            this.panel1.ResumeLayout(false);
+            this.panel3.ResumeLayout(false);
+            this.panel3.PerformLayout();
+            this.panel2.ResumeLayout(false);
+            this.panel2.PerformLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.backPictureBoxBTN)).EndInit();
             this.ResumeLayout(false);
 
         }
 
         #endregion
+
+        private System.Windows.Forms.Panel panel1;
+        private System.Windows.Forms.Panel panel3;
+        private System.Windows.Forms.Button newReportSubmitBTN;
+        private System.Windows.Forms.TextBox reportTitleTextBox;
+        private System.Windows.Forms.Panel panel2;
+        private System.Windows.Forms.PictureBox backPictureBoxBTN;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label label7;
+        private System.Windows.Forms.Label label6;
+        private System.Windows.Forms.Label label5;
+        private System.Windows.Forms.RichTextBox reportDescriptionRichTextBox;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.DateTimePicker dateofIncidentDateTimePicker;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.TextBox locationTextBox;
+        private System.Windows.Forms.CheckBox submitAnonymously;
+        private System.Windows.Forms.Label label8;
+        private System.Windows.Forms.TextBox evidenceTextBox;
+        private System.Windows.Forms.Button chooseFileBTN;
+        private System.Windows.Forms.ComboBox typeOfCorruptionComboBox;
+        private System.Windows.Forms.CheckBox submitNone;
+        private System.Windows.Forms.ListBox evidenceListBox;
     }
 }
--- Reporter/ReporterSubmitNewReport.cs
+++ Reporter/ReporterSubmitNewReport.cs
@@ -12,9 +12,25 @@ namespace SpeakOut___Anti_Corruption_Reporting_System.Reporter
 {
     public partial class ReporterSubmitNewReport : Form
     {
+        List<string> evidenceFiles = new List<string>();
+
         public ReporterSubmitNewReport()
         {
             InitializeComponent();
+        }
+
+        private void chooseFileBTN_Click(object sender, EventArgs e)
+        {
+            OpenFileDialog ofd = new OpenFileDialog();
+            ofd.Multiselect = true;
+            if (ofd.ShowDialog() == DialogResult.OK)
+            {
+                foreach (string file in ofd.FileNames)
+                {
+                    evidenceFiles.Add(file);
+                    evidenceListBox.Items.Add(Path.GetFileName(file));
+                }
+            }
         }
     }
 }
